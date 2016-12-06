9e033f0bf0f49f5127e574c1ba1cdfea0798ff59
Podolske <GAR_napodolske@seattleschools.org>
2016-12-06 09:25:50 -0800

    Add a new portfolio class and the stock object

---
+++ app/src/main/java/com/garfieldcs/gar_jhhua/fantasystocks/ShowPortfolioActivity.java
@@ -0,0 +1,15 @@
+package com.garfieldcs.gar_jhhua.fantasystocks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowPortfolioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_portfolio);
+
+
+    }
+}
---
+++ app/src/main/java/com/garfieldcs/gar_jhhua/fantasystocks/Stock.java
@@ -0,0 +1,7 @@
+package com.garfieldcs.gar_jhhua.fantasystocks;
+
+/**
+ * Created by gar_napodolske on 12/6/2016.
+ */
+public class Stock {
+}
