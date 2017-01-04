a182c6e25686d19e2c2419d56de499447fd4c79a
Hua <GAR_jhhua@seattleschools.org>
2017-01-04 09:11:16 -0800

    Rename stock class and add "get" methods for price

--- app/src/main/java/com/garfieldcs/gar_jhhua/fantasystocks/StockInfo.java
+++ app/src/main/java/com/garfieldcs/gar_jhhua/fantasystocks/StockInfo.java
@@ -3,23 +3,21 @@ package com.garfieldcs.gar_jhhua.fantasystocks;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.os.AsyncTask;
-import android.widget.Toast;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.math.BigDecimal;
+
 
 import yahoofinance.YahooFinance;
 
-/**
- * Created by gar_napodolske on 12/6/2016.
- */
-public class Stock {
+public class StockInfo {
     private String name;
     private String symbol;
     private CheckConnection c;
     private yahoofinance.Stock stock;
 
-    public Stock(String name, Context context) {
+    public StockInfo(String name, Context context) {
         this.name = name;
         c = new CheckConnection(context);
         new CollectDataTask().execute(name);
@@ -28,9 +26,38 @@ public class Stock {
     public String getName(){
         return name;
     }
+
     public String getSymbol(){
         new CollectDataTask().getSymbol();
         return symbol;
+    }
+
+    public Float getPrice() {
+        return Array.getFloat(new CollectDataTask().getInfo(), 0);
+    }
+
+    public Float getChange() {
+        return Array.getFloat(new CollectDataTask().getInfo(), 1);
+    }
+
+    public Float getChangeP() {
+        return Array.getFloat(new CollectDataTask().getInfo(), 2);
+    }
+
+    public Float getHighY() {
+        return Array.getFloat(new CollectDataTask().getInfo(), 3);
+    }
+
+    public Float getLowY() {
+        return Array.getFloat(new CollectDataTask().getInfo(), 4);
+    }
+
+    public Float getHighD() {
+        return Array.getFloat(new CollectDataTask().getInfo(), 5);
+    }
+
+    public Float getLowD() {
+        return Array.getFloat(new CollectDataTask().getInfo(), 6);
     }
 
     //Collects data in a separate thread
@@ -75,6 +102,10 @@ public class Stock {
 
         protected String getSymbol() {
             return symbol;
+        }
+
+        protected String[] getInfo() {
+            return new String[] {price, change, changeP, highY, lowY, highD, lowD};
         }
     }
 }
