dc253b7f67fcdfe354f02851e6529337d191b042
Podolske <GAR_napodolske@seattleschools.org>
2017-01-09 08:53:52 -0800

    Adds ShowPortfolio and User classes

---
+++ app/src/main/java/com/garfieldcs/gar_jhhua/fantasystocks/User.java
@@ -0,0 +1,36 @@
+package com.garfieldcs.gar_jhhua.fantasystocks;
+
+/**
+ * Created by gar_napodolske on 1/4/2017.
+ */
+public class User {
+
+    protected int id;
+    protected String un;
+    protected String email;
+    private String pw;
+    protected int lastID = 000001;
+    protected String teamName;
+    protected int leagueID;
+
+    private void createUser (String username, String contact, String password) {
+        un = username;
+        email = contact;
+        pw = password;
+        id = createNewIdentification(lastID);
+    }
+
+    private int createNewIdentification (int lastID){
+        int newID = lastID + 1;
+        lastID = newID;
+        return newID;
+    }
+
+    protected int findUser (){
+        return id;
+    }
+
+    protected int getTeamName (){
+        return teamName;
+    }
+}
