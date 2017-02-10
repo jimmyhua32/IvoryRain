f6db3522349c30e25ee9c528bceb1e59c98acf5b
Hua <GAR_jhhua@seattleschools.org>
2017-02-10 09:30:29 -0800

    Finished MVP of User class except for declaring file paths

--- app/src/main/java/com/garfieldcs/gar_jhhua/fantasystocks/User.java
+++ app/src/main/java/com/garfieldcs/gar_jhhua/fantasystocks/User.java
@@ -4,60 +4,95 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import yahoofinance.Stock;
 
 public class User {
 
-    protected int id; //Eventually create a random id per user
-    protected String un; //Username
-    //Add email functionality later; not MVP
-    //protected String email;
-    private String pw; //Password
-    private boolean encryptStatus;
-    protected String displayName;
+    protected int id;
+    protected String username;
+    private String password;
     private BufferedReader reader;
     private BufferedWriter writer;
+    private boolean doesExist;
 
-    public User(boolean isNewUser) {
+    private ArrayList<String> usernames;
+    private ArrayList<String> passwords;
+    private ArrayList<Integer> ids;
+
+    public User(String username, String password) {
         File folder = new File("path"); //Add actual path later for testing
         File[] allFiles = folder.listFiles();
-        if (isNewUser) {
-            id = generateID();
-        } else {
-            try {
-                for (int i = 0; i < allFiles.length; i++) {
-                    if (allFiles[i].isFile()) {
-                        reader = new BufferedReader(new FileReader(allFiles[i]));
-                        String currentLine;
-                        
+        doesExist = false;
+        try {
+            for (int i = 0; i < allFiles.length; i++) {
+                if (allFiles[i].isFile()) {
+                    reader = new BufferedReader(new FileReader(allFiles[i]));
+                    String currentLine;
+                    while((currentLine = reader.readLine()) != null) {
+                        Scanner s = new Scanner(currentLine);
+                        usernames.add(s.next());
+                        passwords.add(s.next());
+                        ids.add(s.nextInt());
                     }
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (int i = 0; i < usernames.size(); i++) {
+            if (usernames.get(i).equals(username) && passwords.get(i).equals(password)) {
+                this.username = usernames.get(i);
+                this.password = passwords.get(i);
+                this.id = ids.get(i);
+                doesExist = true;
+            }
+        }
+        if (!doesExist) {
+            boolean generatedID = false;
+            while (!generatedID) {
+                id = generateID();
+                for (int i : ids) {
+                    if (id == i) {
+                        break;
+                    }
+                }
+                generatedID = true;
+            }
+            if (!this.username.equals(username)) {
+                this.username = username;
+                this.password = password;
+                try {
+                    //Path should be id + .txt
+                    writer = new BufferedWriter(new FileWriter(new File("path")));
+                    writer.write(id + " " + username + " " + password);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                doesExist = true;
+            } else {
+                //If username already exists
             }
         }
     }
 
-    //Username and display name are different; the name others see
-    public void createName(String displayName) {
-        this.displayName = displayName;
-    }
+    public boolean doesExist() {
 
-    public String getDisplayName (){
-        return displayName;
+        return false;
     }
 
     public String getUserName() {
         //Add some checks
-        return un;
+        return username;
     }
 
     //Generates an id
     private int generateID() {
-
+        return 0;
     }
 
 
@@ -65,12 +100,12 @@ public class User {
         if (encryptStatus) {
             decryptPW();
         }
-        if (username == un && password == pw) {
+        if (username == this.username && password == this.password) {
             return true;
-        } else if (username != un) {
+        } else if (username != this.username) {
             System.out.println("Wrong user"); //testing purposes
             return false;
-        } else if (password != pw) {
+        } else if (password != this.password) {
             System.out.println("Wrong password"); //again, for testing
             return false;
         }
@@ -84,13 +119,13 @@ public class User {
 
     /* CAN IGNORE EVERYTHING BELOW FOR NOW
        CONTAINS "ENCRYPTION" WHICH REALLY DOESN'T DO ANYTHING YET
-     */
+
 
     //Secures the password
     private void encryptPW() {
         String newPass = "";
 
-        this.pw = newPass;
+        this.password = newPass;
         encryptStatus = true;
     }
 
@@ -98,7 +133,8 @@ public class User {
     private void decryptPW() {
         String oldPass = "";
 
-        this.pw = oldPass;
+        this.password = oldPass;
         encryptStatus = false;
     }
+    */
 }
