99acf961f0617d644ade68769f240fa8ffa14f82
Beard <GAR_debeard@seattleschools.org>
2017-02-10 09:08:37 -0800

    modified portfolio to show an stock names and include assets

--- app/src/main/java/com/garfieldcs/gar_jhhua/fantasystocks/ShowPortfolioActivity.java
+++ app/src/main/java/com/garfieldcs/gar_jhhua/fantasystocks/ShowPortfolioActivity.java
@@ -2,6 +2,8 @@ package com.garfieldcs.gar_jhhua.fantasystocks;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -11,14 +13,25 @@ import yahoofinance.YahooFinance;
 public class ShowPortfolioActivity extends AppCompatActivity {
 
     private String teamName;
-    protected ArrayList Stocks = new ArrayList();
+    protected ArrayList<String> Stocks = new ArrayList<String>();
+    protected ListView list = (ListView) findViewById(R.id.userAssetsList);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_portfolio);
 
+        Stocks.add("Apple");
+        Stocks.add("Google");
+        Stocks.add("Costco");
+        Stocks.add("Yahoo");
+        Stocks.add("Starbucks");
+        Stocks.add("Microsoft");
+        Stocks.add("Amazon");
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,Stocks);
+
+        list.setAdapter(adapter);
     }
 
 }
