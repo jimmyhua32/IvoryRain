c684022577477d16d21f5e7abdd063020164c4e2
Beard <GAR_debeard@seattleschools.org>
2017-02-15 09:28:03 -0800

    modified portfolio activity to populate the listview

--- app/src/main/java/com/garfieldcs/gar_jhhua/fantasystocks/ShowPortfolioActivity.java
+++ app/src/main/java/com/garfieldcs/gar_jhhua/fantasystocks/ShowPortfolioActivity.java
@@ -4,9 +4,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import yahoofinance.YahooFinance;
 
@@ -22,20 +27,45 @@ public class ShowPortfolioActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_portfolio);
 
-        Stocks.add("Apple");
-        Stocks.add("Google");
-        Stocks.add("Costco");
-        Stocks.add("Yahoo");
-        Stocks.add("Starbucks");
-        Stocks.add("Microsoft");
-        Stocks.add("Amazon");
+        HashMap<String, Integer> nameQuantity = new HashMap<>();
+
+        nameQuantity.put("Apple", 3);
+        nameQuantity.put("Google", 4);
+        nameQuantity.put("Costco", 2);
+        nameQuantity.put("Yahoo", 1);
+        nameQuantity.put("Starbucks", 5);
+        nameQuantity.put("Microsoft", 6);
+        nameQuantity.put("Amazon", 6);
+
+        ArrayList<HashMap<String, Integer>> dataList = new ArrayList<>();
 
         Bundle bundle = getIntent().getExtras();
         String username = bundle.getString("Username");
 
+        TextView teamName = (TextView) findViewById(R.id.userTeamName);
+        TextView totalValue = (TextView) findViewById(R.id.TotalAssetValue);
+        TextView bankValue = (TextView) findViewById(R.id.BankAccountValue);
+        TextView investedValue = (TextView) findViewById(R.id.InvestedAssetsValue);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,Stocks);
+        //replace these with variables adding up assets
+        teamName.setText(username);
+        totalValue.setText("$10000");
+        bankValue.setText("$10000");
+        investedValue.setText("$0");
 
+        SimpleAdapter adapter = new SimpleAdapter(this, dataList, R.layout.list_portfolio_stocks,
+                new String[] {"First Line", "Second Line"},
+                new int[] {R.id.list_main_text, R.id.list_quantity});
+
+        Iterator it = nameQuantity.entrySet().iterator();
+
+        while (it.hasNext()) {
+            HashMap<String, Integer> resultsMap = new HashMap<>();
+            Map.Entry pair = (Map.Entry) it.next();
+            resultsMap.put("First Line", pair.getValue().hashCode());
+            resultsMap.put("Second Line", pair.getValue().hashCode());
+            dataList.add(resultsMap);
+        }
         list.setAdapter(adapter);
     }
 
