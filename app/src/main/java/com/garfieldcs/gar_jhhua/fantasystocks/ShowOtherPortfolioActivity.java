5400263a3520cb5b245a073a452be148e2d6c3c4
Beard <GAR_debeard@seattleschools.org>
2017-04-28 10:38:45 -0700

    started making leaderboard clickable

--- app/src/main/java/com/garfieldcs/gar_jhhua/fantasystocks/ShowOtherPortfolioActivity.java
+++ app/src/main/java/com/garfieldcs/gar_jhhua/fantasystocks/ShowOtherPortfolioActivity.java
@@ -4,10 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ShowOtherPortfolioActivity extends AppCompatActivity {
+    private User user;
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_other_portfolio);
+
+        Bundle bundle = getIntent().getExtras();
+        username = bundle.getString("Username");
     }
 }
