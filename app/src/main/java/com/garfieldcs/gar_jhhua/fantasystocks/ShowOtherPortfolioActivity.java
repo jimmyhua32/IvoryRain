f12b82d343353db09ec46eb5b3a37dcd81fa0619
Beard <GAR_debeard@seattleschools.org>
2017-05-01 09:13:23 -0700

    modified SOPA to fill arraylist of ownedstocks

--- app/src/main/java/com/garfieldcs/gar_jhhua/fantasystocks/ShowOtherPortfolioActivity.java
+++ app/src/main/java/com/garfieldcs/gar_jhhua/fantasystocks/ShowOtherPortfolioActivity.java
@@ -3,10 +3,13 @@ package com.garfieldcs.gar_jhhua.fantasystocks;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class ShowOtherPortfolioActivity extends AppCompatActivity {
     private User user;
     private Integer userID;
     private String password;
+    ArrayList<String> userStocks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +18,10 @@ public class ShowOtherPortfolioActivity extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         userID = bundle.getInt("UserID");
-
+        User userToView = new User(userID, getApplicationContext());
+        OwnedStocks ownedStocks = new OwnedStocks(userID, getApplicationContext());
+        userStocks = ownedStocks.getAsset();
+        
 
     }
 }
